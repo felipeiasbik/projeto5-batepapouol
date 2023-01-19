c90253d214896aed81ae8821a02704e4de3c9b0d
Felipe Pires Iasbik <felipeiasbik@hotmail.com>
2023-01-18 21:34:23 -0300

    Adicionadas mensagens do API no HTML

--- scripts.js
+++ scripts.js
@@ -15,9 +15,52 @@ function meustatus(){
     testeStatus.catch(statusErro);
 }
 
+setInterval(meustatus, 5000);
+
+function mensagensOk(pegarMsgs){
+    console.log(pegarMsgs);
+
+    for (let i = 0; i < pegarMsgs.data.length; i++){
+        let horarioMsg = pegarMsgs.data[i].time;
+        let nomeA = pegarMsgs.data[i].from;
+        let nomeB = pegarMsgs.data[i].to;
+        let textoMsg = pegarMsgs.data[i].text;
+
+        if (textoMsg === "entra na sala..."){
+            document.querySelector('ul').innerHTML = `
+            <li class="entrou-saiu">
+                <p class="texto"><span class="hora">(09:21:48)</span><span>${nomeA}</span> ${textoMsg}</p>
+            </li>
+            ` + document.querySelector('ul').innerHTML;
+        } else if (nomeB === "Todos"){
+            document.querySelector('ul').innerHTML = `
+            <li class="msg">
+                <p class="texto"><span class="hora">(09:21:48)</span><span>${nomeA}</span> para <span>${nomeB}</span>: ${textoMsg}</p>
+            </li>
+            ` + document.querySelector('ul').innerHTML;
+        } else {
+            document.querySelector('ul').innerHTML = `
+            <li class="reservado">
+                <p class="texto"><span class="hora">(09:21:48)</span><span>${nomeA}</span> reservadamente para <span>${nomeB}</span>: ${textoMsg}</p>
+            </li>
+            ` + document.querySelector('ul').innerHTML;
+        }
+    }
+}
+
+function mensagensErro(){
+    alert('Erro ao exibir mensagens. Entre na sala novamente.');
+    entrarNaSala();
+}
+
 function mostrarSala(){
     document.querySelector('ul').classList.remove('esconder');
-    setInterval(meustatus, 5000);
+
+    const pegarMensagens = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+
+    pegarMensagens.then(mensagensOk);
+    pegarMensagens.catch(mensagensErro);
+
 }
 
 function processaRespostaErro(erro){
@@ -26,8 +69,8 @@ function processaRespostaErro(erro){
 }
 
 function processaResposta(resposta){
-    alert('Comunicação efetuada com sucesso!');
-    mostrarSala();
+    alert('Seja bem vindo(a) ao Bate Papo UOL!');
+    setInterval(mostrarSala,3000);
 }
 
 function entrarNaSala(){
